17f05eda513f1654d10e3ca09a0587187a3cc662
Aman <Palaman2626@gmail.com>
2023-02-06 22:39:59 +0530

    new files added

--- Lab/Assignment 1 and 2.R
+++ Lab/Assignment 1 and 2.R
@@ -62,3 +62,81 @@ i=my_list[[1]] %% 2 == 0
 my_list[[1]][i]
 sum(my_list[[1]][i])
 
+L <-list(c(1,4,2,5,7)) #creating a vector inside the list
+L
+L[[1]] = NULL #clear all the elements in the vector
+
+rm(L) #delete list
+L
+
+######################2 Question##########################
+
+#add new entry in D
+
+key <- c(1,2,3,4,5) #entering the keys in dictionary
+values <- c(5.6,7.8,6.6,8.7,7.7) #entering the value for keys
+df <- data.frame(key,values) #making dictionary provided with keys and values
+df
+df[nrow(df)+1,] <- c(8,8.8) #adding the entry in the new row after the last row with key 8 and value 8.8
+
+#remove key=2
+df <- subset(df,key!=2) #just like telling to take subset where keys is not equal to 2
+df
+
+#to check if 6 key is present in D
+subset(df,key==6)
+
+#count the number of elements in D
+nrow(df)
+
+#to add all the values in D
+sum(df$values)
+
+#to update the value of key 3
+df$values[df$key == 3] <- 7.1
+df
+
+#to clear dictionary
+rm(df)
+df
+
+
+#################3 Question#################################
+
+#given 2 sets
+s1 <-c(10,20,30,40,50,60)
+s2 <-c(40,50,60,70,80,90)
+
+#add 2 elements 55,66 in s1
+s1 <- c(s1,55,66)
+s1
+
+#remove 10,30 from set1
+s1 <- s1[-c(1,3)] #enter the index of the element in the c not the values  #silly mistake
+s1
+
+print(40 %in% s1) #check if 40 is present in s1
+
+#to do union of 2 sets
+union_set <- union(s1,s2)
+union_set
+
+#to do intersection of 2 sets
+intersected_sets <- intersect(s1,s2)
+intersected_sets
+
+#difference between sets
+diff_set <- setdiff(s1,s2)
+diff_set
+
+
+######################4 Question#############################
+
+#to print 100 random strings length between 6 and 8
+strings <- sapply(1:100,function(x){
+  paste(sample(LETTERS,size = sample(10:15,1)),collapse = "")
+})
+strings
+
+
+
